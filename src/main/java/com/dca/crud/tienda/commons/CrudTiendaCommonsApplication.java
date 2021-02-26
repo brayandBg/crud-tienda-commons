f3142d2fc184a15dbc67de592a4107674f32992f
brayandBg <76263880+brayandBg@users.noreply.github.com>
2021-02-26 10:51:23 -0500

    Creacion proyecto commons de la API crud-tienda

---
+++ src/main/java/com/dca/crud/tienda/commons/CrudTiendaCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.dca.crud.tienda.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTiendaCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTiendaCommonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dca/crud/tienda/commons/CrudTiendaCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dca.crud.tienda.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTiendaCommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
